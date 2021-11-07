0e65d5a5d5c6f90d3a8c17504f7ab47fc333f0ce
Knivemare <35490770+Knivemare@users.noreply.github.com>
2021-11-07 12:12:51 +0100

    Login Support + Navbar fixed

--- components/AppMetaTags.js
+++ components/AppMetaTags.js
@@ -8,17 +8,17 @@ export default function AppMetaTags() {
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
             <link rel="shortcut icon" href="/favicon.ico" />
-            <link rel="manifest" href="/favicon/site.webmanifest"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+            <link rel="manifest" href="/pwa/manifest.json"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/ico/favicon-16x16.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/ico-32x32.png"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/ico/apple-touch-icon.png"/>
 
-            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000"/>
-            <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <link rel="mask-icon" href="/ico/safari-pinned-tab.svg" color="#000000"/>
+            <link rel="shortcut icon" href="/ico/favicon.ico" />
             <meta name="msapplication-TileColor" content="#000000" />
             <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-            <meta name="theme-color" content="#000" />
-            <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+            <meta name="theme-color" content="#000"/>
+            {/*  <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> */}
         </Head>
     )
 }
